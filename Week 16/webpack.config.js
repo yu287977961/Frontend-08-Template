468fc6b019c6a69bc8515354fbfd11a1ff0f2f8b
XiLinYu <287977961@qq.com>
2021-04-18 20:54:16 +0800

    perf：第十六课作业

---
+++ Week 16/webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    mode:'development',  //production
+    entry:'./main.js',
+    
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                use:{
+                    loader:"babel-loader",
+                    options: {
+                        presets:["@babel/preset-env"],
+                        plugins: [["@babel/plugin-transform-react-jsx",{pragma:"createElement"}]]
+                    }
+                }
+            }
+        ]
+    },
+
+    target:"web",
+    devServer: {
+        port: 3001,
+        compress:true,
+        open:true,
+        hot: true
+    },
+
+}
